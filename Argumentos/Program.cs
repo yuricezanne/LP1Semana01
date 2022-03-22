e089fd5af4cfc0a1c79111394e10e9f35004b133
yuricezanne <yuricezanne@live.com>
2022-03-22 20:21:07 +0000

    Exercício 1 - Argumentos

---
+++ Argumentos/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string[] arguments = { "LP1", "Semana1" };
+
+        for(int i = 0; i < arguments.Length; i++)
+        {
+            Console.WriteLine(arguments[i]);
+        }
+    }
+}
