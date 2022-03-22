80ca9fecacf62dfd0fdc0e9b30ea16075f11831c
yuricezanne <yuricezanne@live.com>
2022-03-22 20:33:13 +0000

    Commit 1

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int inteiro1 = 1;
+        int inteiro2 = 2;
+        string string1 = "LP1";
+
+        Console.WriteLine(string.Concat(inteiro1, inteiro2, string1));
+   
+    }
+}
