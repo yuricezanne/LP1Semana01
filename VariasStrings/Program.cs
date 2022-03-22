5f26a20db69ce001af9cfa96e9a66aa54135ea4e
yuricezanne <yuricezanne@live.com>
2022-03-22 21:01:59 +0000

    Exercício 5 -  Semana 1 - Strings

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string string1 = "ULHT";
+        string string2 = string.Concat(string1, " - Videojogos");
+        string string3 = $"Universidade {string1}";
+        string string4 = string.Format("O valor da string1 é {0}", string1);
+        Console.WriteLine(@"Teste, {0}", string1);
+        Console.WriteLine(string1);
+        Console.WriteLine(string2);
+        Console.WriteLine(string3);
+        Console.WriteLine(string4);
+    }
+}
